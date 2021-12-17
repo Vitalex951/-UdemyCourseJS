187a7641bd55ba30b65973675ca2072f64e1c5b1
Vitalex <vitalex951@gmail.com>
2021-12-17 15:45:18 +0300

    Friday

--- lesson8/lesson8.js
+++ lesson8/lesson8.js
@@ -1,5 +1,5 @@
 
-let num = prompt('Введите число', 0);
+/* let num = prompt('Введите число', 0); */
 
 /* if (num < 49) {
     console.log("неверно")
@@ -13,7 +13,7 @@ let num = prompt('Введите число', 0);
 
 (num == 50) ? console.log("Верно") : console.log("Неверно"); */
 
-switch (true) {
+/* switch (true) {
     case (num < 49):
         console.log("Неверно");
         break;
@@ -30,5 +30,55 @@ switch (true) {
         console.log("Что-то пошло не так");
         break;
 
-}
+} */
 
+/* function first() {
+       setTimeout( function() {
+        console.log(1);
+    }, 500 );
+}
+function second() {
+    console.log(2);
+}
+first();
+second();
+
+function learnJS(lang, callbacl) {
+    console.log("Я учу " + lang);
+    callbacl();
+}
+ function done(){
+    console.log("Я прошел 3 урок")
+ }
+learnJS("Java SCRiot", done) */
+
+l/* et options = {
+    width: 1024,
+    height: 1024,
+    name: "test"
+};
+
+console.log(options.name);
+
+options.bool = false;
+options.coolors = {
+    border: "black",
+    bg: "red"
+}
+delete options.bool;
+console.log (options);
+
+for (let key in options) {
+    console.log("Свойство " + key + " имеет значени "+ options[key]);
+}console.log (Object.keys(options)); */
+
+function hello(name) {
+    let phrase = `Hello, ${name}!`;
+  
+    say(phrase);
+  }
+  
+  function say(phrase) {
+    alert(`** ${phrase} **`);
+  }
+  
