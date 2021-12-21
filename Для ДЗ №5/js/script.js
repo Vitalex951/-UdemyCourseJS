54f822cda3244ac74f7d46d16f41d41fe2fc1465
Vitalex <vitalex951@gmail.com>
2021-12-21 21:04:39 +0300

    homework6

--- Для ДЗ №5/js/script.js
+++ Для ДЗ №5/js/script.js
@@ -1,3 +1,30 @@
-let menuFive = document.querySelector(".menu-item");
+let menuFive = document.querySelector(".menu"),
+    liFive = document.createElement("li"),
+    bodyNew = document.querySelector('body'),
+    mainDiv = document.querySelector(".title"),
+    deletDiv = document.querySelector(".adv");
+    parentalDiv = document.querySelectorAll(".column"),
+    menuItem = document.getElementsByClassName(".menu-item");
+    /* apple = prompt("Какое у вас отношение к технике Apple?"),
+    appleAnswer = document.querySelector("#prompt"); */
+    
+    console.log("menuItem");
 
-console.log(menuFive);
+
+
+    
+menuFive.appendChild(liFive);
+liFive.classList.add("menu-item");
+liFive.textContent = "Пятый пункт";
+
+menuFive.insertBefore(menuItem[2], menuItem[1]);
+
+bodyNew.style.background = "url('img/apple_true.jpg')";
+
+
+mainDiv.innerHTML = "Мы продаем только подлинную технику Apple";
+
+
+parentalDiv[1].removeChild(deletDiv);
+
+/* appleAnswer.innerHTML = apple; */
