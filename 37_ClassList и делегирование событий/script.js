3daea2a7124b61e1ae4d96963d3be78dfbe20bce
Vitalex951 <vitalex951@gmail.com>
2022-01-11 16:19:23 +0300

    lesson 41 TIMER

---
+++ 37_ClassList и делегирование событий/script.js
@@ -0,0 +1,36 @@
+"use strict";
+let btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
+
+
+// btns[0].addEventListener('click', () => {
+  // if (!btns[1].classList.contains('red')) {
+  //   btns[1].classList.add('red');
+  // } else {
+  //   btns[1].classList.remove('red');
+  // }
+
+  btns[1].classList.toggle('red');
+});
+
+// console.log(btns[0].className);
+
+// wrapper.addEventListener("click", (event) => {
+//     if (event.target && event.target.tagName == "BUTTON") {
+//       console.log('Hello')
+//     }
+// });
+// btns.forEach(btn ()=> {
+//   btn.addEventListener('click', () => {
+//     console.log('hello');
+//   });
+// });
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
+
+wrapper.addEventListener("click", (event) => {
+  if (event.target && event.target.matches ('button', 'red')) {
+    console.log('Hello');
+  }
+});
