6aca1eeed99d9013eaf995c9671a1297c675ae8f
Vitalex951 <vitalex951@gmail.com>
2022-01-18 16:13:14 +0300

    'AJAX'

--- Food/js/script.js
+++ Food/js/script.js
@@ -1,19 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
-// Табы
+    // Табы
     const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
-    function hideTabContent () {
-        tabsContent.forEach (item => {
+    function hideTabContent() {
+        tabsContent.forEach(item => {
             item.style.display = 'none';
         });
 
-        tabs.forEach (item => {
+        tabs.forEach(item => {
             item.classList.remove('tabheader__item_active');
-        } );
+        });
     }
-    
+
     function showTabContent(i = 0) {
         tabsContent[i].style.display = 'block';
         tabs[i].classList.add('tabheader__item_active');
@@ -23,80 +23,80 @@ window.addEventListener('DOMContentLoaded', () => {
     showTabContent();
 
     tabsParent.addEventListener('click', (event) => {
-       const target = event.target;
-       if (target && target.classList.contains('tabheader__item')) { 
-           tabs.forEach((item, i) => {
-            if (target == item) {
-                hideTabContent();
-                showTabContent(i);
-            }
-           });
-       }
+        const target = event.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
     });
 
-// Таймер
+    // Таймер
 
-const deadline = '2022-01-17';
+    const deadline = '2022-01-17';
 
-function getTimeRemaining (endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000*60*60*24)),
-          hours = Math.floor((t / (1000*60*60) % 24)),
-          minutes = Math.floor((t/1000/60) % 60),
-          seconds = Math.floor((t/1000) % 60);
-    
-    return {
-        'total': t,
-        days,
-        hours,
-        minutes,
-        seconds
-    };
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
 
-}
+        return {
+            'total': t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
 
-function getZero (num) {
-    if (num >=0 && num < 10) {
-        return `0${num}`;
-    } else {
-        return num;
     }
-}
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
 
 
-function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-        days = document.querySelector('#days'),
-        hours = document.querySelector('#hours'),
-        minutes = document.querySelector('#minutes'),
-        seconds = document.querySelector('#seconds'),
-        timeInterval = setInterval(updateClock, 1000);
-        
-        updateClock ();
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = document.querySelector('#days'),
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
 
-        function updateClock () {
+        updateClock();
+
+        function updateClock() {
             const t = getTimeRemaining(endtime);
 
             days.innerHTML = getZero(t.days);
             hours.innerHTML = getZero(t.hours);
             minutes.innerHTML = getZero(t.minutes);
             seconds.innerHTML = getZero(t.seconds);
-            
+
             if (t.total <= 0) {
                 clearInterval(timeInterval);
             }
         }
-}
-    setClock('.timer', deadline);
+    }
+    // setClock('.timer', deadline);
 
     //Modal
 
     const modalTrigger = document.querySelectorAll('[data-modal'),
-          modalClose = document.querySelector('[data-close]'),
-          modal = document.querySelector('.modal');
+        modalClose = document.querySelector('[data-close]'),
+        modal = document.querySelector('.modal');
 
-          console.log(modalTrigger);
-          console.log(modalClose);
+    console.log(modalTrigger);
+    console.log(modalClose);
 
 
     modalTrigger.forEach((item) => {
@@ -116,7 +116,7 @@ function setClock(selector, endtime) {
         document.body.style.overflow = '';
     }
 
-    modal.addEventListener('click' , (i) => {
+    modal.addEventListener('click', (i) => {
         console.log(i.target);
         if (i.target === modal) {
             closeModal();
@@ -126,7 +126,7 @@ function setClock(selector, endtime) {
     modalClose.addEventListener('click', closeModal);
 
     document.addEventListener('keydown', (e) => {
-        if (e.code === "Escape"&&  modal.style.display == 'block') {
+        if (e.code === "Escape" && modal.style.display == 'block') {
             console.log('11111');
             closeModal();
         }
@@ -143,6 +143,77 @@ function setClock(selector, endtime) {
 
     window.addEventListener('scroll', showModalByScroll);
 
-      
-       
+    //Используем классы для карточек
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+           
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> $/день</div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container',
+
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container",
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container",
+    ).render();
+
+
+
 });
